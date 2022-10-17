ed2be8b7371d9eba2f86946b3817c0e3f00482a6
marcrine <marcrinemm@gmail.com>
2022-10-17 17:41:10 +0300

    updates

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,9 +6,9 @@ import sys
 
 if __name__ == "__main__":
     """Function to display employee todo list."""
-    url = "https://jsonplaceholder.typicode.com"
-    user = requests.get(url + "/users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "/todos/", params={"userId": sys.argv[1]}).json()
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
     completed = [t.get("title") for t in todos if t.get("completed") is True]
     print("Employee {} is done with tasks({}/{}):".format(
