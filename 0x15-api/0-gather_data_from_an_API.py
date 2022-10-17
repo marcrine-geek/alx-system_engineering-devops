7e9212421cac26aa2bf7b8d8bdbe866de6f80e8a
marcrine <marcrinemm@gmail.com>
2022-10-17 15:24:11 +0300

    changes task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,7 +3,9 @@
 import requests
 import sys
 
+
 if __name__ == "__main__":
+    """Function to display employee todo list."""
     url = "https://jsonplaceholder.typicode.com/"
     user = requests.get(url + "users/{}".format(sys.argv[1])).json()
     todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
@@ -12,4 +14,3 @@ if __name__ == "__main__":
     print("Employee {} is done with tasks({}/{}):".format(
         user.get("name"), len(completed), len(todos)))
     [print("\t {}".format(c)) for c in completed]
-    
